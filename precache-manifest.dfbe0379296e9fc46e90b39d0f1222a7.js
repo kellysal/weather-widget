f199565cf219db5313a5a3e5f9c3297af3f15e83
Kelly Salamack <kellysalamackdesign@gmail.com>
2019-09-20 11:04:38 -0400

    Updates

---
+++ precache-manifest.dfbe0379296e9fc46e90b39d0f1222a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c7828f0d12039f1f0d6f896948d27d2",
+    "url": "/weather-widget/index.html"
+  },
+  {
+    "revision": "a0116a6ce4eff923e6bd",
+    "url": "/weather-widget/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "c85cda0a7c53e41e1e80",
+    "url": "/weather-widget/static/css/main.857c2b9f.chunk.css"
+  },
+  {
+    "revision": "a0116a6ce4eff923e6bd",
+    "url": "/weather-widget/static/js/2.d29811ba.chunk.js"
+  },
+  {
+    "revision": "c85cda0a7c53e41e1e80",
+    "url": "/weather-widget/static/js/main.e30d5a87.chunk.js"
+  },
+  {
+    "revision": "fa1bc510912ba27ce900",
+    "url": "/weather-widget/static/js/runtime-main.c86d97d8.js"
+  }
+]);
